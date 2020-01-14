be54f1272fe71219802d33cf16ede01f8566f620
nearnshaw <nearnshaw@decentraland.org>
2020-01-14 13:59:37 -0300

    hover feedback

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,20 @@
-import { OpenBoard, OpenLerp } from "modules/openchest";
-import { MoveGems, GemData, gems, gemValues, GrowGems, gridToScene } from "./modules/gems";
-import { SwipeDetection, Directions } from "./modules/swiping";
-import { BoardData } from "./modules/board";
+import { OpenBoard, OpenLerp } from 'modules/openchest'
+import {
+  MoveGems,
+  GemData,
+  gems,
+  gemValues,
+  GrowGems,
+  gridToScene
+} from './modules/gems'
+import { SwipeDetection, Directions } from './modules/swiping'
+import { BoardData } from './modules/board'
 
 // object to get buttonUp and buttonDown events
 const input = Input.instance
 
 // object to get user position and rotation
 const camera = Camera.instance
-
 
 //////////////////////////////
 // Helper functions
@@ -21,14 +27,13 @@ function openChest() {
       chestOpen.play()
       chestLightOpen.play()
       // play sounds
-      if (board.tutorialDone){
+      if (board.tutorialDone) {
         clearGems()
         addRandomGem()
-      }
-      else{
+      } else {
         doTutorial()
       }
-      
+
       break
     case false:
       chestClose.play()
@@ -68,18 +73,16 @@ const spawner = {
     let shapeIndex = gemValues.indexOf(val)
     ent.addComponentOrReplace(gemModels[shapeIndex])
 
-    let t = ent. getComponentOrCreate(Transform)
+    let t = ent.getComponentOrCreate(Transform)
     t.scale.setAll(0.5)
     t.position = gridToScene(x, y)
 
-    let td = ent. getComponentOrCreate(GemData)
+    let td = ent.getComponentOrCreate(GemData)
     td.reset(val, x, y)
 
     engine.addEntity(ent)
   }
 }
-
-
 
 function addRandomGem() {
   var emptyCells: number[] = [
@@ -108,7 +111,7 @@ function addRandomGem() {
     emptyCells.splice(index, 1)
   }
 
-  if (emptyCells.length > 0){
+  if (emptyCells.length > 0) {
     var index = ~~(Math.random() * emptyCells.length)
     var cell = emptyCells[index]
     var newValue = Math.random() < board.fourProbability ? 4 : 2
@@ -126,16 +129,16 @@ function shiftBlocks(direction: Directions) {
     var currentRow: any[] = [null, null, null, null]
     for (let gem in gems.entities) {
       let gemPos = gems.entities[gem].getComponent(GemData).nextPos
-      let pos :number
-      switch (direction){
+      let pos: number
+      switch (direction) {
         case Directions.UP:
           if (gemPos.x == row) {
-            pos = (gemPos.y * -1 ) + 3
+            pos = gemPos.y * -1 + 3
           }
           break
         case Directions.RIGHT:
           if (gemPos.y == row) {
-            pos = (gemPos.x * -1 ) + 3
+            pos = gemPos.x * -1 + 3
           }
           break
         case Directions.DOWN:
@@ -145,13 +148,13 @@ function shiftBlocks(direction: Directions) {
           break
         case Directions.LEFT:
           if (gemPos.y == row) {
-           pos = gemPos.x
+            pos = gemPos.x
           }
           break
-      }  
+      }
       currentRow[pos] = gems.entities[gem]
     }
-    
+
     // go over each tile in row (or column)
     for (var target = 0; target < board.size; ++target) {
       let alreadyMerged = false
@@ -160,16 +163,18 @@ function shiftBlocks(direction: Directions) {
         if (currentRow[tile]) {
           let gemData = currentRow[tile].getComponent(GemData)
           // if target tile is empty, shift gems
-          if (currentRow[target] == null ||  alreadyMerged == true) {
+          if (currentRow[target] == null || alreadyMerged == true) {
             moveGem(gemData, direction)
             hasChanged = true
           } else {
             // if target tile has a gem, check if it can be merged
             let targetData = currentRow[target].getComponent(GemData)
-            if (gemData.val == targetData.val && 
-                targetData.willUpgrade == false && 
-                targetData.willDie == false &&
-                blocked == false) {
+            if (
+              gemData.val == targetData.val &&
+              targetData.willUpgrade == false &&
+              targetData.willDie == false &&
+              blocked == false
+            ) {
               moveGem(gemData, direction)
               mergeGems(gemData, targetData)
               alreadyMerged = true
@@ -182,7 +187,7 @@ function shiftBlocks(direction: Directions) {
     }
   }
 
-  if (hasChanged && board.tutorialDone){
+  if (hasChanged && board.tutorialDone) {
     addRandomGem()
     hasLost()
   }
@@ -206,72 +211,83 @@ function moveGem(gemData: GemData, direction: Directions) {
   gemData.lerp = 0
 }
 
-function mergeGems(gemData: GemData, targetData: GemData){
+function mergeGems(gemData: GemData, targetData: GemData) {
   gemData.willDie = true
   targetData.willUpgrade = true
   targetData.val *= 2
   // Check if won
-  if (targetData.val == 2048){
+  if (targetData.val == 2048) {
     win()
   }
 }
 
+function hasLost() {
+  // check if board is full
+  if (gems.entities.length < 16) {
+    return
+  }
 
-function hasLost(){
-    // check if board is full
-    if (gems.entities.length < 16) {return} 
-
-    var canMove: boolean = false;
-    var fullBoard: any[][] = [[null, null, null, null],[null, null, null, null],[null, null, null, null],[null, null, null, null]]
-    for (let gem in gems.entities) {
-        let gemData = gems.entities[gem].getComponent(GemData)
-        if (!gemData.willDie){
-          fullBoard[gemData.nextPos.y][gemData.nextPos.x] = gemData.val
-        }
-        
+  var canMove: boolean = false
+  var fullBoard: any[][] = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null]
+  ]
+  for (let gem in gems.entities) {
+    let gemData = gems.entities[gem].getComponent(GemData)
+    if (!gemData.willDie) {
+      fullBoard[gemData.nextPos.y][gemData.nextPos.x] = gemData.val
     }
+  }
 
-    log(fullBoard)
+  log(fullBoard)
 
-     for (var r = 0; r < board.size; ++r) {
-       for (var c = 0; c < board.size; ++c) {
-        canMove = fullBoard[r][c] == null? true: canMove
-        if (r < 3){
-          canMove = fullBoard[r][c] == fullBoard[r+1][c]? true: canMove
-         }
-         if (c < 3){
-          canMove = fullBoard[r][c] == fullBoard[r][c+1]? true: canMove
-         }
-       }
-     }
-    if (canMove == false){
-      loose()
+  for (var r = 0; r < board.size; ++r) {
+    for (var c = 0; c < board.size; ++c) {
+      canMove = fullBoard[r][c] == null ? true : canMove
+      if (r < 3) {
+        canMove = fullBoard[r][c] == fullBoard[r + 1][c] ? true : canMove
+      }
+      if (c < 3) {
+        canMove = fullBoard[r][c] == fullBoard[r][c + 1] ? true : canMove
+      }
     }
-
+  }
+  if (canMove == false) {
+    loose()
+  }
 }
 
 // Board
 const boardWrapper = new Entity()
-boardWrapper.addComponent(new Transform({
-  position: new Vector3(8, 0, 10.5),
-  scale: new Vector3(0.05, 0.05, 0.05)
-}))
+boardWrapper.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 10.5),
+    scale: new Vector3(0.05, 0.05, 0.05)
+  })
+)
 boardWrapper.addComponent(new OpenLerp())
 engine.addEntity(boardWrapper)
 
-
-function doTutorial(){
+function doTutorial() {
   let instructions = new Entity()
   instructions.setParent(boardWrapper)
-  instructions.addComponent(new TextShape("Drag gems by clicking and dragging anywhere. \nMerge gems until you reach the highest value!"))
+  instructions.addComponent(
+    new TextShape(
+      'Drag gems by clicking and dragging anywhere. \nMerge gems until you reach the highest value!'
+    )
+  )
   instructions.getComponent(TextShape).fontSize = 1
   instructions.getComponent(TextShape).shadowColor = Color3.Gray()
   instructions.getComponent(TextShape).shadowOffsetY = 1
   instructions.getComponent(TextShape).shadowOffsetX = -1
-  instructions.addComponent(new Transform({
-    position: new Vector3(0, 3, -1),
-    scale: new Vector3(4, 4, 1)
-  }))
+  instructions.addComponent(
+    new Transform({
+      position: new Vector3(0, 3, -1),
+      scale: new Vector3(4, 4, 1)
+    })
+  )
   engine.addEntity(instructions)
 
   let buttonMaterial = new Material()
@@ -281,35 +297,43 @@ function doTutorial(){
   button.addComponent(new PlaneShape())
   button.addComponent(buttonMaterial)
   button.setParent(boardWrapper)
-  button.addComponent(new Transform({
-    position: new Vector3(0, -2.5, -0.5)
-  }))
-  button.addComponent(new OnClick(e => {
-    engine.removeEntity(button)
-    engine.removeEntity(instructions)
-    board.tutorialDone = true
-    clearGems()
-    addRandomGem()
-  }))
+  button.addComponent(
+    new Transform({
+      position: new Vector3(0, -2.5, -0.5)
+    })
+  )
+  button.addComponent(
+    new OnPointerDown(
+      e => {
+        engine.removeEntity(button)
+        engine.removeEntity(instructions)
+        board.tutorialDone = true
+        clearGems()
+        addRandomGem()
+      },
+      { button: ActionButton.POINTER, hoverText: 'Start' }
+    )
+  )
 
   engine.addEntity(button)
 
   let buttonText = new Entity()
   buttonText.setParent(button)
-  buttonText.addComponent(new TextShape("Let's start!" ))
+  buttonText.addComponent(new TextShape("Let's start!"))
   buttonText.getComponent(TextShape).fontSize = 1
-  buttonText.addComponent(new Transform({
-	  position: new Vector3(0, 0 , -0.3)
-  }))
+  buttonText.addComponent(
+    new Transform({
+      position: new Vector3(0, 0, -0.3)
+    })
+  )
   engine.addEntity(buttonText)
 
   spawner.spawnGem(2, 1, 2)
   spawner.spawnGem(2, 3, 2)
-
 }
 
-function clearGems(){
-  for (const gem of gems.entities){
+function clearGems() {
+  for (const gem of gems.entities) {
     engine.removeEntity(gem)
   }
 }
@@ -323,10 +347,12 @@ const board = new BoardData()
 // Island
 const island = new Entity()
 island.addComponent(new GLTFShape('models/Island.gltf'))
-island.addComponent(new Transform({
-  position: new Vector3(8, 0, 10.5),
-  rotation: Quaternion.Euler(0, 270, 0)
-}))
+island.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 10.5),
+    rotation: Quaternion.Euler(0, 270, 0)
+  })
+)
 engine.addEntity(island)
 
 // Banner
@@ -337,19 +363,23 @@ bannerMaterial.texture = bannerTexture
 const banner = new Entity()
 banner.addComponent(bannerMaterial)
 banner.addComponent(new PlaneShape())
-banner.addComponent(new Transform({
-  position: new Vector3(8, 8, 10.5),
-  scale: new Vector3(6, 6, 6)
-}))
+banner.addComponent(
+  new Transform({
+    position: new Vector3(8, 8, 10.5),
+    scale: new Vector3(6, 6, 6)
+  })
+)
 engine.addEntity(banner)
 
 // Chest
 const chest = new Entity()
-chest.addComponent(new Transform({
-  position: new Vector3(8, 0.2, 10.5),
-  rotation: Quaternion.Euler(0, 270, 0),
-  scale: new Vector3(0.8, 0.8, 0.8)
-}))
+chest.addComponent(
+  new Transform({
+    position: new Vector3(8, 0.2, 10.5),
+    rotation: Quaternion.Euler(0, 270, 0),
+    scale: new Vector3(0.8, 0.8, 0.8)
+  })
+)
 chest.addComponent(new GLTFShape('models/Chest.gltf'))
 let chestAnimator = new Animator()
 chest.addComponent(chestAnimator)
@@ -360,16 +390,21 @@ chestClose.looping = false
 chestAnimator.addClip(chestOpen)
 chestAnimator.addClip(chestClose)
 chest.addComponent(
-  new OnClick(e => {
-    openChest()
-  })
+  new OnPointerDown(
+    e => {
+      openChest()
+    },
+    { button: ActionButton.POINTER, hoverText: 'Open' }
+  )
 )
 
 engine.addEntity(chest)
 
 // Chest Light
 const chestLight = new Entity()
-chestLight.addComponent(new Transform({rotation: Quaternion.Euler(0,180,0)}))
+chestLight.addComponent(
+  new Transform({ rotation: Quaternion.Euler(0, 180, 0) })
+)
 chestLight.setParent(chest)
 chestLight.addComponent(new GLTFShape('models/Light.gltf'))
 let chestLightAnimator = new Animator()
@@ -385,11 +420,13 @@ engine.addEntity(chestLight)
 // Map
 const map = new Entity()
 map.setParent(boardWrapper)
-map.addComponent(new Transform({
-  position: new Vector3(0, 1, 0),
-  rotation: Quaternion.Euler(0, 180, 0),
-  scale: new Vector3(2, 2, 2)
-}))
+map.addComponent(
+  new Transform({
+    position: new Vector3(0, 1, 0),
+    rotation: Quaternion.Euler(0, 180, 0),
+    scale: new Vector3(2, 2, 2)
+  })
+)
 map.addComponent(new GLTFShape('models/Map.gltf'))
 engine.addEntity(map)
 
@@ -434,20 +471,14 @@ function win() {
   log('YOU WON!')
 }
 
-
-
 ///////////////////////////
 // Add systems
-
-
 
 engine.addSystem(new OpenBoard(boardWrapper))
 
 engine.addSystem(new MoveGems(gemModels))
 
 engine.addSystem(new GrowGems())
-
-
 
 // MISSING:
 
